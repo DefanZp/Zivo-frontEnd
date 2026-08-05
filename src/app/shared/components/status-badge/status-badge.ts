fd65311ac4342b34babf9c54a408db8096ee1c75
DefanZp <defanzainp@gmail.com>
2026-08-05 09:46:46 +0700

    learn configuration object pattern on status badge

--- src/app/shared/components/status-badge/status-badge.ts
+++ src/app/shared/components/status-badge/status-badge.ts
@@ -1,57 +1,49 @@
 import { Component, computed, input } from '@angular/core';
 
+interface StatusConfig {
+    label: string;
+    class: string;
+}
+
 @Component({
   selector: 'app-status-badge',
   imports: [],
   templateUrl: './status-badge.html',
   styleUrl: './status-badge.css',
 })
+
 export class StatusBadge {
 
   status = input.required<string>();
 
-  badgeClass = computed (() => {
-    switch (this.status()) {
+  private statusConfig: Record<string, StatusConfig> = {
 
-      case 'pending':
-      return 'bg-yellow-100 text-yellow-800';
+    pending: { 
+      label: 'Pending', 
+      class: 'bg-yellow-100 text-yellow-800' 
+    },
+    processing: { 
+      label: 'Processing', 
+      class: 'bg-blue-100 text-blue-800' 
+    },
+    completed: { 
+      label: 'Completed', 
+      class: 'bg-green-100 text-green-800' 
+    },
+    cancelled: { 
+      label: 'Cancelled', 
+      class: 'bg-red-100 text-red-800' 
+    },
 
-      case 'processing':
-        return 'bg-blue-100 text-blue-800';
+  };
+  
+  config = computed(() => {
+    return this.statusConfig[this.status()] ?? {
 
-      case 'completed':
-        return 'bg-green-100 text-green-800';
+      label: this.status(),
 
-      case 'cancelled':
-        return 'bg-red-100 text-red-800';
-
-      default:
-        return 'bg-gray-100 text-gray-800';
-
-      }
+      class: 'bg-gray-100 text-gray-800'
+    }
   })
-
-  label = computed(() => {
-
-  switch (this.status()) {
-
-    case 'pending':
-      return 'Pending';
-
-    case 'processing':
-      return 'Processing';
-
-    case 'completed':
-      return 'Completed';
-
-    case 'cancelled':
-      return 'Cancelled';
-
-    default:
-      return this.status();
-
-  }
-
-});
 
 }
