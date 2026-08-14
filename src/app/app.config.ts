467b10b6cb72fc7c98ef943b586e92f7e304974f
DefanZp <defanzainp@gmail.com>
2026-08-14 16:43:38 +0700

    - add config, when user go to another page, restore to top

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,20 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { tokenInterceptor } from './core/interceptors/token.interceptors';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), 
+    provideRouter(
+      routes,
+      // ke paling atas halaman ketika pindah page
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'enabled',
+        anchorScrolling: 'enabled',
+      })
+    ), 
     // provideClientHydration(withEventReplay()),
     provideHttpClient(
       withFetch(),
